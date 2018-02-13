971b04d3158412890a3ba2402f919b7f7a588790
fouad <Chamma.fouad@gmail.com>
2018-02-13 14:59:02 +1100

    updated enterkey function

--- pixel.js
+++ pixel.js
@@ -1,9 +1,12 @@
 $set = $('.colorButton')
 
-$set.on('click', function(event){
-
-	 debugger
+$set.on('click keypress', function(event){
 	 $(".brushBox").css("background-color", $('input').val());
-
-
 })
+
+$('input').on('keypress', function(e) {
+    var code = e.keyCode || e.which;
+    if(code==13){
+        $(".brushBox").css("background-color", $('input').val());
+    }
+});
